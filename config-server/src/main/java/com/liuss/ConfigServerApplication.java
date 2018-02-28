e2df93559f61147fcd38f71d03a52b963f836fb7
liushuisheng <buptlss@gmail.com>
2018-02-28 10:49:24 +0800

    add origin code

---
+++ config-server/src/main/java/com/liuss/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package com.liuss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	/**
+	 * http请求地址和资源文件映射如下:
+	 *
+	 *\/{application}\/{profile}[\/{label}]
+	 *\/{application}-{profile}.yml
+	 *\/{label}\/{application}-{profile}.yml
+	 *\/{application}-{profile}.properties
+	 *\/{label}\/{application}-{profile}.properties
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
